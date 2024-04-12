0d186c35eec151fad9c3d4f86c2440235580f870
Alexey Kaziev <kazievab@gmail.com>
2024-04-13 01:41:38 +0300

    Fix vite base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import scalaJSPlugin from "@scala-js/vite-plugin-scalajs";
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: "/gods-and-glory-assistant/",
   plugins: [
     scalaJSPlugin(),
     react()
